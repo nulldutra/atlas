dd316cf7c1a42dbb69b98500cfeb6dd71ac1cc88
Gabriel M. Dutra <haskelldutra@gmail.com>
2025-01-18 15:33:22 -0300

    main: Start HTTP server

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"atlas/config"
+	"atlas/proxy"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config := config.NewConfig()
+
+	fmt.Println(config.DenyIPList)
+	fmt.Println(config.DenyHTTPHeader)
+	fmt.Println(config.Backend)
+
+	proxy := proxy.NewProxy(config.Backend, config.DenyIPList)
+	http.HandleFunc("/", proxy.Server)
+
+	fmt.Println("Starting WaF atlas service..")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
