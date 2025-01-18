5d748e724e8358c3314862c67eab7eb4cb7af215
Gabriel M. Dutra <haskelldutra@gmail.com>
2025-01-18 15:31:02 -0300

    config: Add config

---
+++ config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DenyIPList     []string `mapstructure:"denyIPList"`
+	DenyHTTPHeader []string `mapstructure:"denyHTTPHeader"`
+	Backend        []string `mapstructure:"backend"`
+}
+
+func NewConfig() *Config {
+	viper.SetConfigFile("config.yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		panic(err)
+	}
+
+	return &config
+}
